ba54e4dcc10dcdc88f9467eece116ea2305f9978
lilianbaz <7318431+lilianbaz@users.noreply.github.com>
2022-12-01 16:01:38 +0100

    Day 1

---
+++ day01/day01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+inputFile = open("input.txt", "r")
+inputLines = inputFile.readlines()
+
+elves = [0]
+elfNumber = 0
+
+for line in inputLines:
+    line = line.replace("\n", "")
+    if line == "":
+        elfNumber = elfNumber + 1
+        elves.append(0)
+    else:
+        elves[elfNumber] = elves[elfNumber] + int(line)
+
+elves.sort(reverse = True)
+
+print("The Elf carrying the most calories have " + str(elves[0]) + " calories.")
+print("The top three Elves have " + str(elves[0] + elves[1] + elves[2]) + " calories.")
